a579898a8d4a2e314cf9e6b75cd37b0044542d95
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-01 17:57:11 -0500

    Application of teaching model

--- app.py
+++ app.py
@@ -8,12 +8,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 prompt = """show 5 cities has receive funding from the Urban Area Security Initiative UASI"""
 
 #generate a response
+#response = openai.Completion.create(
+#            model="text-davinci-003",
+#            prompt=prompt,
+#            temperature=0.6,
+#            max_tokens=256,
+#        )
+
+
+
+#generate a response using fine tuned model  (recommend at least 100 entries)
 response = openai.Completion.create(
-            model="text-davinci-003",
+            model="davinci:ft-personal-2023-02-01-22-49-33",
             prompt=prompt,
             temperature=0.6,
             max_tokens=256,
         )
+
 
 #extract the notes from the response
 notes = response.choices[0].text
@@ -23,3 +34,7 @@ with open('testresults.txt', mode='w') as file_object:
 
 #print the notes
 print(notes)
+
+
+
+
