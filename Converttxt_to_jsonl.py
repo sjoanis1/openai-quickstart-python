cb60468a45ed61cd1f295e23f9d6b3b72bc7b264
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-03 13:24:20 -0500

    Prepare JSONL data file

---
+++ Converttxt_to_jsonl.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import openai
+
+#os.system("Python --version")
+#os.system("openai tools fine_tunes.prepare_data -f C:\Users\steph\OneDrive\Documents\GitHub\RWT_AI_FILES\data\datatest.txt","Y","Y","Y")
+
+#result = subprocess.run(['Python', '--version'],shell = True)
+
+# not working
+#result = subprocess.run(['openai','tools','fine_tunes.prepare_data','-f','C:\Users\steph\OneDrive\Documents\GitHub\RWT_AI_FILES\data\datatest.txt'],shell=true)
+#print(result.stdout)
+
+
+
+#  command to convert from Txt to JSONL
+#       openai tools fine_tunes.prepare_data -f C:\Users\steph\OneDrive\Documents\GitHub\RWT_AI_FILES\data\datatest.txt
+
+
+
+
+
+
