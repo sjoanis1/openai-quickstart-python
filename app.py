cdbf35e80807cce3a02853a5a7293c0440159efe
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-03 17:44:07 -0500

    Modify to have open ai call at top of code.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
-            temperature=0.6,
+            temperature=0,
             max_tokens=350,
         )
         return redirect(url_for("index", result=response.choices[0].text))
