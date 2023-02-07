b357bb4dab9d00c3b675f3f12a68361e2c683dc7
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-07 11:45:39 -0500

    Modify AI Assistant for multiline input.

--- app.py
+++ app.py
@@ -6,27 +6,33 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 
+
 @app.route("/", methods=("GET", "POST"))
+
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        main_question = request.form["question1"] 
+        print(main_question)
         response = openai.Completion.create(
 #            model="text-davinci-003",
-#            model="text-curie-001",         
-            model="curie:ft-personal-2023-02-06-21-28-47",            
-            prompt=generate_prompt(animal),
+            model="text-curie-001",         
+#            model="curie:ft-personal-2023-02-06-21-28-47",            
+            prompt=generate_prompt(main_question),
             temperature=0,
             max_tokens=350,
         )
+    
         return redirect(url_for("index", result=response.choices[0].text))
+
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-def generate_prompt(animal):
+
+def generate_prompt(main_question):
     return """
     {}   
 """.format(
-        animal.capitalize()
+        main_question.capitalize()
     )
 
 
@@ -34,7 +40,9 @@ def generate_prompt(animal):
 #notes = response.choices[0].text
 ##Add response to a .txt file
 #with open('testresults.txt', mode='w') as file_object:
-#    print((prompt,"|",notes), file=file_object)
+#    print((mainquestion,"|",test_output), file=file_object)
+   
+
 
 #print the notes
 #print(notes)
