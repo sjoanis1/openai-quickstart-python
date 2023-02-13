09c0a226a0d3a0289f30bc1c3f8c6c5dd858d4a3
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-13 18:07:10 -0500

    Addition of loop to go through txt file questions.

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ openai.organization = "org-s5xRdnxv3xUnpGDRaIRKDC6H"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 #openai.Model.list()
 
+#open file containing questions.
+f = open("Data\\UASI_Questions.txt", "r") 
+
+for x in f:
+    print(x)
+
 #define the prompt
-prompt = """show 5 cities has receive funding from the Urban Area Security Initiative UASI"""
+    prompt = x
+
+#prompt =  """show 5 cities has receive funding from the Urban Area Security Initiative UASI"""
+
+
 
 #generate a response
 #response = openai.Completion.create(
@@ -18,8 +28,9 @@ prompt = """show 5 cities has receive funding from the Urban Area Security Initi
 
 
 #generate a response using fine tuned model  (recommend at least 100 entries)
-response = openai.Completion.create(
-            model="davinci:ft-personal-2023-02-01-22-49-33",
+    response = openai.Completion.create(
+          #  model="davinci:ft-personal-2023-02-01-22-49-33",
+            model="text-davinci-003",
             prompt=prompt,
             temperature=0.6,
             max_tokens=256,
@@ -27,13 +38,13 @@ response = openai.Completion.create(
 
 
 #extract the notes from the response
-notes = response.choices[0].text
+    notes = response.choices[0].text
 #Add response to a .txt file
-with open('testresults.txt', mode='w') as file_object:
-    print((prompt,"|",notes), file=file_object)
+    with open('testresults.txt', mode='w') as file_object:
+        print((prompt,"|",notes), file=file_object)
 
 #print the notes
-print(notes)
+    print(notes)
 
 
 
