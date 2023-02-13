f6d7caec66dc147fef9eeb2952342ed63911915b
sjoanis1 <sjoanis@sjperformancecoach.com>
2023-02-13 18:15:06 -0500

    Loop to answer questions from a file.

--- app.py
+++ app.py
@@ -12,20 +12,6 @@ for x in f:
 #define the prompt
     prompt = x
 
-#prompt =  """show 5 cities has receive funding from the Urban Area Security Initiative UASI"""
-
-
-
-@app.route("/", methods=("GET", "POST"))
-
-def index():
-    if request.method == "POST":
-        main_question = request.form["question1"] 
-        print(main_question)
-
-        temp = float(request.form["temperature"])        
-        print(temp)
-
 #generate a response using fine tuned model  (recommend at least 100 entries)
     response = openai.Completion.create(
           #  model="davinci:ft-personal-2023-02-01-22-49-33",
@@ -35,13 +21,11 @@ def index():
             max_tokens=256,
         )
     print(response.choices[0].text)
-    return redirect(url_for("index", result=response.choices[0].text))      
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
+  
 
 #extract the notes from the response
     notes = response.choices[0].text
+
 #Add response to a .txt file
     with open('testresults.txt', mode='w') as file_object:
         print((prompt,"|",notes), file=file_object)
